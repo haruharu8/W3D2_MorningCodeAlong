a50bdfe168af1565b1069d2f3d78f5285e6c6e21
Kelly Siu <ksiu@LABA.PSWAS.LOCAL>
2023-08-15 10:09:45 -0700

    commit 3

--- functions_practice.js
+++ functions_practice.js
@@ -24,11 +24,48 @@ const isDivisible = (num) => {
     if (num % 3 === 0 && num % 5 === 0) {
         return true
     } else {
-        return true
+        return false
     }
 }
 
 // let groupOneSize = 125;
 
-let groupOneisGood = isDivisible(125);
+let groupOneisGood = isDivisible(15);
 
+console.log(groupOneisGood);
+
+if (groupOneisGood) {
+
+}
+
+let myWords = "What is the longest word in this string?"
+
+const findLongestWord = (sentence) => {
+    // what if sentence is not a string or is an empty string
+    
+    // return the longest word
+
+    // 1. split string into individual words
+    let arrayOfStrings = sentence.split(' ')
+    console.log(arrayOfStrings);
+    arrayOfStrings[0] // "what"     let currentLongest = "what"
+    arrayOfStrings[1] // "words"
+    arrayOfStrings[2] // "are"
+    arrayOfStrings[3] // "longest"
+    arrayOfStrings[4] // "here"
+    // 2. loop through array and find the longest word
+    let currentLongest = ''
+    // i    0 1 2 3 4 X
+    for (let i=0; i<arrayOfStrings.length; i++) {
+        // is this word longer than the currentLongestWord
+        if (arrayOfStrings[i].length > currentLongest.length) {
+            currentLongest = arrayOfStrings[i]
+        }
+    }
+
+    return currentLongest;
+}
+
+findLongestWord("what is the longest word here?")
+findLongestWord("what is the longest word here?")
+findLongestWord("what is the longest word here?")
