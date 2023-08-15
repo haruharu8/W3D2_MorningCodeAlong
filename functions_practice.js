bc7de2db9630ae810910950dea6006e7e3733f5a
Kelly Siu <ksiu@LABA.PSWAS.LOCAL>
2023-08-15 09:29:49 -0700

    first commmit

---
+++ functions_practice.js
@@ -0,0 +1,17 @@
+const answerOne = "lorem ipsum"
+const answerTwo = "This is answer two"
+const answerThree = "lorem ipsum three"
+
+let count = 0;
+
+// write a function that will count the length of 3 strings
+
+const returnCount = (ans1, ans2, ans3) => {
+    return ans1.length + ans2.length + ans3.length
+}
+
+let count1 = returnCount(answerOne, answerTwo, answerThree);
+
+let count2 = returnCount("hello", "im", "Chase");
+
+console.log(count1);
